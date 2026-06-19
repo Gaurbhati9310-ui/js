32689c3c5690ae7f721454cda8d136f7c266ea1e
Gaurbhati9310-ui <gaurbhati9310@gmail.com>
2026-06-19 13:28:12 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,29 @@
+ "use strict"; // treat all js code as newer version 
+
+// alert("hello")
+// alert(3+3)  // we are using node.js not browser 
+
+console.log(3+3);
+console.log("gaur"); //code readability should be high 
+ 
+let name ="gaurav "
+let age = 18
+let isLoggedIn = false 
+
+// number => 2 to power 53 
+// bigint 
+// string => "" 
+//boolean => true / false 
+// null => standalone value 
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "gaurav");
+console.log(typeof null); //object 
+console.log(typeof undefined); //undefined 
+
+
+
